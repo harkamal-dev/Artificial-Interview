49a44f63572de82de2312dfb11d6765d5867a568
Harkamal Singh <hs976034@gmail.com>
2024-11-11 01:14:29 +0530

    Homepage design added

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
 export default function IndexPage() {
+	const navigate = useNavigate();
+
+	const handleClickTry = () => {
+		navigate("/dashboard")
+	}
 	return (
-		<div className="flex justify-center items-center">
-			<h1 className="text-3xl">HOMEPAGE</h1>
+		<div className="flex items-center justify-center lg:justify-normal h-[600px] p-4 lg:p-14">
+			{/* <h1 className="text-3xl">HOMEPAGE</h1> */}
+
+			<div>
+				<h1 className="text-xl lg:text-2xl font-semibold mb-6">Artificial Interview</h1>
+				<h1 className="font-extrabold text-4xl lg:text-8xl ">Elevate your</h1>
+				<h1 className="font-extrabold text-4xl lg:text-8xl ">
+					tech <span className="text-primary"> interviews</span>
+				</h1>
+
+				<Button className="mt-12 bg-primary-foreground text-secondary-foreground" onClick={handleClickTry}>
+					Try it out <ArrowRight />
+				</Button>
+			</div>
 		</div>
 	);
 }
